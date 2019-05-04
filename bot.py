3dbec040d66a54a77c6ec9af2d3beb3bdba826e5
Andrea Soprani <andrea.soprani96@gmail.com>
2019-05-04 17:53:57 +0200

    Bot fully implemented

--- bot.py
+++ bot.py
@@ -1,21 +1,133 @@
+# Standard libraries
 import time
 import json
-import telepot
-from telepot.loop import MessageLoop
-import utility
-from datetime import date
+import datetime
 from collections import OrderedDict
 
+# Bot related libraries
+import telepot
+from telepot.loop import MessageLoop
+
+# HTML parsing related libraries
+import requests
+from bs4 import BeautifulSoup
+
+
+def sendBook(chat_id, book):
+    
+    # Build the message
+    message = ""
+    for key in settings["attribute_names"].keys():
+        message += settings["attribute_names"][key] + ": " + str(book[key]) + "\n"
+    
+    # Send the book
+    bot.sendPhoto(chat_id, book["imageURL"], caption=message)
+
+def sendBookToAll(book):
+    # Retrieve chats
+    with open("chats.json") as f:
+        chats = json.load(f)
+    
+    # Send book to every chat
+    for chat in chats:
+        sendBook(chat["chat_id"], book)
+
+def update():
+    # Load today books
+    with open("today_books.json") as f:
+        todayBooks = json.load(f)
+    
+    # Get today date and confront
+    today = datetime.date.today()
+    
+    # If the last update was from yesterday, set todayBooks as blank and to today
+    if todayBooks["date"] != str(today):
+        todayBooks["date"] = str(today)
+        todayBooks["books"] = []
+        
+        with open("today_books.json", "w") as f:
+            json.dump(todayBooks, f, indent=4, separators=(',', ': '))
+    
+    # GET request
+    content = requests.get(settings["url"]).content
+
+    # BS data structure
+    soup = BeautifulSoup(content, features="lxml")
+
+    # Retrieve books
+    productList = soup.findAll("li", "productListItem")
+
+    # Check date and get info
+    for product in productList:
+
+        # Check date
+        dateString = product.find("li", "releaseDateLabel").find("span").contents[0]
+        book_date = datetime.date(*[int(s) for s in dateString.split() if s.isdigit()][::-1])
+        if book_date != today:
+            break
+    
+        # Get info
+        book_title = product.get("aria-label") #.encode('latin1').decode('utf-8')
+        book_author = product.find("li", "authorLabel").find("a").contents[0] #.encode('latin1').decode('utf-8')
+        book_narrator = product.find("li", "narratorLabel").find("a").contents[0] #.encode('latin1').decode('utf-8')
+        book_runtime = product.find("li", "runtimeLabel").find("span").contents[0].replace("Durata:  ","")
+        book_imageURL = product.find("img", "bc-image-inset-border").get("src")
+
+        # Create book dict
+        book = {
+            "title": book_title,
+            "author": book_author,
+            "narrator": book_narrator,
+            "runtime": book_runtime,
+            "date": str(book_date),
+            "imageURL": book_imageURL
+        }
+        
+        # Add new books to the books list and send it to everyone
+        if book not in todayBooks["books"]:
+            todayBooks["books"].append(book)
+            sendBookToAll(book)
+    
+    # Update today_books file
+    with open('today_books.json', 'w') as f:
+        json.dump(todayBooks, f, indent=4, separators=(',', ': '))
+
 def start(chat_id, command):
-    chats.append(chat_id)
-    print(chats)
-    print("\n")
+    with open("chats.json") as f:
+        chats = json.load(f)
+    
+    chat = next((item for item in chats if item["chat_id"]==chat_id), False)
+    
+    if not chat:
+        chats.append({
+            "chat_id": chat_id,
+            "active": True
+        })
+    else:
+        chat["active"] = True
+
+    with open('chats.json', 'w') as f:
+        json.dump(chats, f, indent=4, separators=(',', ': '))
+    
     bot.sendMessage(chat_id, command["message"])
 
 def stop(chat_id, command):
-    chats.remove(chat_id)
-    print(chats)
-    print("\n")
+    with open("chats.json") as f:
+        chats = json.load(f)
+    
+    chat = next((item for item in chats if item["chat_id"]==chat_id), False)
+    
+    if not chat:
+        chats.append({
+            "chat_id": chat_id,
+            "active": False
+        })
+    else:
+        chat["active"] = False
+
+    with open('chats.json', 'w') as f:
+        json.dump(chats, f, indent=4, separators=(',', ': '))
+        
     bot.sendMessage(chat_id, command["message"])
     
 def help(chat_id, command):
@@ -26,19 +138,18 @@ def help(chat_id, command):
         
     bot.sendMessage(chat_id, help_message)
 
-def new(chat_id, command):
-    new_message = command["message"]
+def today(chat_id, command):
+    # Initial today books message
+    today_books_message = command["message"]
+    bot.sendMessage(chat_id, today_books_message)
     
-    global updated
-    global todayBooks
+    # Load today books
+    with open("today_books.json") as f:
+        todayBooks = json.load(f)
     
-    if updated != date.today():
-        todayBooks = utility.getNewBooks(settings["url"])
-        updated = date.today()
-    
-    new_message += utility.messageBuilder(todayBooks, settings["attribute_names"])
-    
-    bot.sendMessage(chat_id, new_message)
+    # Send every book in today books
+    for book in todayBooks["books"]:
+        sendBook(chat_id, book)
 
 def handle(msg):
     content_type, chat_type, chat_id, date, message_id = telepot.glance(msg, long=True)
@@ -61,7 +172,6 @@ def handle(msg):
         if item["command"] == text:
             func = item["function"]
             globals()[func](chat_id, item)
-
 
 def main():
     
@@ -89,23 +199,15 @@ def main():
     global bot
     bot = telepot.Bot(TOKEN)
     
-    # Chats where the bot is activated.
-    global chats
-    chats = []
-    
-    # Books of the day   
-    global todayBooks
-    todayBooks = utility.getNewBooks(settings["url"])
-    
-    # Day of the last update of todayBooks
-    global updated
-    updated = date.today()
-    
+    # Message handler loop
     MessageLoop(bot, handle).run_as_thread()
     print("Listening...")
     
+    # Update loop
     while(1):
-        time.sleep(10)
+        update()
+        print("Update - " + str(datetime.datetime.now()) + "\n")
+        time.sleep(settings["seconds_between_updates"])
 
 if __name__ == "__main__":
     main()
--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-from collections import OrderedDict
-
-settings_file = open("bot_settings.json")
-settings = json.load(settings_file, object_pairs_hook=OrderedDict)
-
-for key in settings["attribute_names"].keys():
-    print(key)
